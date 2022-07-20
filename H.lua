a350705e661621a8f46c4eeede5cefd07ea13bb9
TheCubed1 <97459356+TheCubed1@users.noreply.github.com>
2022-07-20 18:40:48 -0500

    Create H.lua

---
+++ H.lua
@@ -0,0 +1,26 @@
+wait(2.5)
+  if game.PlaceId == 277751860 then --EpicMinigames
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Epic%20Minigames/EpicMinigamesGUI"))()
+  elseif game.PlaceId == 6468323505 then --Spellbound
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Spellbound%20RPG/SpellboundGui"))()
+  elseif game.PlaceId == 281489669 then --2WizardTycoon
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Wizard%20Tycoon%20-%202%20Player/2pWizardTycoonGUI"))()
+  elseif game.PlaceId == 3625110776 then --EliminationTower
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Elimination%20Tower/EliminationTowerGui"))()
+  elseif game.PlaceId == 7313926139 then --ManicMining
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Manic%20Mining/ManicMiningGUI"))()
+  elseif game.PlaceId == 6951762614 then --UFOSimulator
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/UFO%20Simulator/UFOSimulatorGUI"))()
+  elseif game.PlaceId == 137885680 then --ZombieRush
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Zombie%20Rush/ZombieRushGUI"))()
+  elseif game.PlaceId == 8054462345 then --MichaelsZombies
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Michael's%20Zombies/MichaelsZombiesGUI"))()
+  elseif game.PlaceId == 2248355837 then --ZombieKillingSimulator
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Zombie%20Killing%20Simulator/ZombieKillingSimulatorGUI"))()
+  elseif game.PlaceId == 5874381161 then --NoobInvasion
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Noob%20Invasion/NoobInvasionGUI"))()
+  elseif game.PlaceId == 5171093784 then --DontPressTheButton4
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/SlamminPig/rblxgames/main/Don't%20Press%20The%20Button%204/DontPressTheButton4GUI"))()
+  else
+  game.Players.localPlayer:Kick(Game Not Supported! You Were Kicked For Your Safety.)
+end
